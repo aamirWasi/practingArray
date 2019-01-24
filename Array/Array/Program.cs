ad02cc7a6483ab902692c39923095af1d0394aa1
aamir <wasi3aamir@gmail.com>
2019-01-25 01:21:51 +0600

    Foreach

---
+++ Array/Array/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int [] favNums = new int[3];
+            favNums[0] = 23;
+            favNums[1] = 45;
+            favNums[2] = 21;
+            for(int i=0;i<favNums.Length;i++){
+}
+        }
+    }
+}
---
+++ array2/array2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] randArray = {1, 2, 3, 4};
+            printArray(randArray, "ForEach");
+
+        }
+        static void printArray(int[] intArray,string mess)
+        {
+            foreach(int k in intArray)
+            {
+                Console.WriteLine("{0} {1}",mess,k);
+            }
+        }
+    }
+}
